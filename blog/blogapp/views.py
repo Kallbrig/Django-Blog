13a01894ad97e4c6a87c2e2beb45c12236504e7a
Kristopher Allbright <kallbright@troy.edu>
2020-04-07 14:25:17 -0500

    Added Pagination to the homepage and to the new User posts page.

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -21,7 +22,23 @@ class PostListView(ListView):
     # by default, Django is looking for one named object_list. we are changing it below.
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
 
+
+class UserPostListView(ListView):
+    model = Post
+    # looks for template with naming structure of <app>/<model>_<viewtype>.html
+    # in this case it's looking for blogapp/post_list
+    template_name = 'blogapp/user_posts.html'
+    # in the template, we are already using the {{ posts }} name to get our posts.
+    # by default, Django is looking for one named object_list. we are changing it below.
+    context_object_name = 'posts'
+
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
